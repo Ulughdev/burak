f356d9eebf59832fd5bb308d3ecca734b2c5f4e5
Ulughdev <ulugh0618@gmail.com>
2024-03-06 15:47:18 -0800

    feat: task - I

--- src/train.ts
+++ src/train.ts
@@ -1,18 +1,9 @@
+function getDigits(mix: string): Number{
+    let a:number = parseFloat(mix.replace(/[^\d\.]*/g, ''));;
+     return a;
+};
 
-function getPositive(numbers: number[]): string{ 
-    let a = ("");
-    for(let number of numbers){
-        if(number > 0){
-            a = a + number;
-
-        }
-    }
-    return a.split("").join("")
-
-}
-
-console.log(getPositive([1, -4, 2]));
-
+console.log("javob: ", getDigits("m21nh2jhh667"));
 
 /* PROJECT STANDARDS
  - logging standards   --controller 
