650b6a0fbb19a99ded953168c86602772c87ee49
Ulughdev <ulugh0618@gmail.com>
2024-03-13 17:27:44 -0700

    fix: modify restaurant controller processSiginup and processLogin

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -3,7 +3,7 @@ import {T} from "../libs/types/common";
 import MemberService from "../models/member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
-import Errors, { Message } from "../libs/types/Errors";
+import Errors, { HttpCode, Message } from "../libs/types/Errors";
 
 const memberService = new MemberService();
 
@@ -47,8 +47,12 @@ restaurantController.processSignup = async (req: AdminRequest, res: Response) =>
     try {
         console.log("processSignup");
         //console.log("body: ", req.body);
+        const file = req.file;
+        if(!file)
+        throw new Errors(HttpCode.BAD_REQUEST, Message.SOMETHING_WENT_WRONG);
 
         const newMember: MemberInput = req.body; 
+        newMember.memberImage = file?.path;
         newMember.memberType = MemberType.RESTAURANT;
         const result = await memberService.processSignup(newMember);
 
@@ -56,7 +60,7 @@ restaurantController.processSignup = async (req: AdminRequest, res: Response) =>
 
          req.session.member = result;
          req.session.save(function(){
-            res.send(result);
+            res.redirect("/admin/product/all");
          });
 
     }catch (err) {
@@ -79,7 +83,7 @@ restaurantController.processLogin = async (req: AdminRequest, res: Response) => 
 
          req.session.member = result;
          req.session.save(function(){
-            res.send(result);
+            res.redirect("/admin/product/all");
          });
     }catch (err) {
         console.log("ERROR, processLogin", err);
