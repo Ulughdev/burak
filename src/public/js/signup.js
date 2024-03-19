b6ba272f96d05eb33848566cf6a866ef574b189d
Ulughdev <ulugh0618@gmail.com>
2024-03-19 15:27:15 -0700

    feat: signup process frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,26 @@
 console.log("Signup frontend javascript file");
+
+$(function() {});
+
+function validateSignupForm() {
+    const memberNick = $(".member-nick").val();
+    const memberPhone = $(".member-phone").val();
+    const memberPassword = $(".member-password").val();
+    const confimPassword = $(".confirm-password").val();
+
+    if(
+    memberNick === "" || 
+    memberPhone === "" || 
+    memberPassword === "" || 
+    confimPassword === ""
+    ){
+        alert("Please insert all required inputs");
+        return false;
+    }
+
+    if(memberPassword !== confimPassword){
+        alert("Password differs, please check!")
+        return false;
+    }
+    //console.log("memberNick: ", memberNick);
+}
